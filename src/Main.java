d40d0f21e432da7eed6f85d2056d88c1eae59ae3
Guilherme <parzivalwwatts366@gmail.com>
2025-08-02 12:22:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner teclado = new Scanner(System.in);
 
-        //mensagem de boas-vindas:
-        System.out.println("Welcome to the Gordura Market!");
+        //welcome message:
+        System.out.println("Welcome to your Market!");
 
-        //dados do produto:
+        //product data:
         Stock product;
         product= new Stock();
         System.out.println("First, you need to insert the info about the product: ");
@@ -19,22 +19,22 @@ public class Main {
         System.out.println("Please, insert the quantity of the product: ");
         product.q = teclado.nextInt();
 
-        //impressão dos dados
+        //data printing
         System.out.printf("Name: %s%n",product.n);
         System.out.printf("Price: %.2f $%n",product.p);
         System.out.printf("Quantity in stock: %d%n",product.q);
 
-        //preço inicial
+        //initial price
         product.totalValueInStock();
         System.out.printf("Product data: %s,"+" %d units,"+" Total: $ %.2f%n", product.n, product.q, product.totalValueInStock());
 
-        //adição
+        //add
         System.out.println("Enter the number of products to be added in stock: ");
         int q = teclado.nextInt();
         product.addProducts(q);
         System.out.printf("Updated data: %s,"+" %d units,"+" Total: $ %.2f%n", product.n, product.q, product.totalValueInStock());
 
-        //remoção
+        //remove
         System.out.println("Enter the number of products to be removed from stock: ");
         q = teclado.nextInt();
         product.removeProducts(q);
