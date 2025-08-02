6242ed6ecf2f12802bdbc121f4c4f43b0789637e
Guilherme <parzivalwwatts366@gmail.com>
2025-08-02 18:52:26 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,36 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner teclado = new Scanner(System.in);
 
-        //welcome message
-        System.out.println("Welcome to your Market!");
+        //mensagem de boas-vindas:
+        System.out.println("Welcome to the Gordura Market!");
 
-        //product data
-        Stock product;
-        product = new Stock();
-        System.out.println("First, you need to insert the info about the product");
+        //dados do produto:
+        System.out.println("First, you need to insert the info about the product: ");
         System.out.println("Please, insert the name of the product: ");
-        product.n = teclado.next();
+        String name = teclado.nextLine();
         System.out.println("Please, insert the price of the product: ");
-        product.p = teclado.nextDouble();
+        double price = teclado.nextDouble();
         System.out.println("Please, insert the quantity of the product: ");
-        product.q = teclado.nextInt();
+        int quantity = teclado.nextInt();
 
-        //data printing
+        Estoque product = new Estoque(name, price, quantity);
+
+        //impressão dos dados
         System.out.printf("Name: %s%n",product.n);
         System.out.printf("Price: %.2f $%n",product.p);
         System.out.printf("Quantity in stock: %d%n",product.q);
 
-        //initial price
+        //preço inicial
         product.totalValueInStock();
         System.out.printf("Product data: %s,"+" %d units,"+" Total: $ %.2f%n", product.n, product.q, product.totalValueInStock());
 
-        //add
+        //adição
         System.out.println("Enter the number of products to be added in stock: ");
         int q = teclado.nextInt();
         product.addProducts(q);
         System.out.printf("Updated data: %s,"+" %d units,"+" Total: $ %.2f%n", product.n, product.q, product.totalValueInStock());
 
-        //remove
+        //remoção
         System.out.println("Enter the number of products to be removed from stock: ");
         q = teclado.nextInt();
         product.removeProducts(q);
