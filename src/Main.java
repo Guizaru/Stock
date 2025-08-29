c9c2ac9f0887a0bd671c1212427f619975fb93ef
Guizaru <parzivalwwatts366@gmail.com>
2025-08-29 19:57:04 -0300

    polishing it

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,32 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner teclado = new Scanner(System.in);
 
-        //mensagem de boas-vindas:
-        System.out.println("Welcome to the Market haha!");
+        System.out.println("Welcome to the Market!");
 
-        //dados do produto:
-        System.out.println("First, you need to insert the info about the product: ");
-        System.out.println("Please, insert the name of the product: ");
+        System.out.println("First, you need to insert the info about the product.");
+        System.out.print("Please, insert the name of the product: ");
         String name = teclado.nextLine();
-        System.out.println("Please, insert the price of the product: ");
+        System.out.print("Please, insert the price of the product: ");
         double price = teclado.nextDouble();
-        Stock product = new Stock(name, price);
+        System.out.print("Please, insert the quantity of the product: ");
+        int quantity = teclado.nextInt();
+        Stock product = new Stock(name, price, quantity);
 
-        //impressão dos dados
         System.out.printf("Name: %s%n",product.enterName);
         System.out.printf("Price: %.2f $%n",product.enterPrice);
         System.out.printf("Quantity in stock: %d%n",product.enterQuantity);
 
-        //preço inicial
         product.totalValueInStock();
         System.out.printf("Product data: %s,"+" %d units,"+" Total: $ %.2f%n", product.enterName, product.enterQuantity, product.totalValueInStock());
 
         //adição
-        System.out.println("Enter the number of products to be added in stock: ");
+        System.out.print("Enter the number of products to be added in stock: ");
         int q = teclado.nextInt();
         product.addProducts(q);
         System.out.printf("Updated data: %s,"+" %d units,"+" Total: $ %.2f%n", product.enterName, product.enterQuantity, product.totalValueInStock());
 
         //remoção
-        System.out.println("Enter the number of products to be removed from stock: ");
+        System.out.print("Enter the number of products to be removed from stock: ");
         q = teclado.nextInt();
         product.removeProducts(q);
         System.out.printf("Updated data: %s,"+" %d units,"+" Total: $ %.2f%n", product.enterName, product.enterQuantity, product.totalValueInStock());
